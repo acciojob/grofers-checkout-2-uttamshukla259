092a7a2efa76cf0b75abf0a9923b267ea66f0831
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-03-30 22:40:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+// Get all the elements with data-ns-test="prices"
+const pricesElements = document.querySelectorAll('[data-ns-test="prices"]');
+
+// Calculate the sum of all prices
+let total = 0;
+for (let i = 0; i < pricesElements.length; i++) {
+  total += parseFloat(pricesElements[i].innerText);
+}
+
+// Create a new row for the total and add it to the table
+const newRow = document.createElement('tr');
+const totalCell = document.createElement('td');
+totalCell.innerText = `Total: $${total.toFixed(2)}`;
+newRow.appendChild(totalCell);
+document.querySelector('table').appendChild(newRow);
+
+// Set the data-ns-test attribute for the total cell
+totalCell.setAttribute('data-ns-test', 'grandTotal');
